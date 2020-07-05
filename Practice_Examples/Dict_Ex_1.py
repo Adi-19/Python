b22b1c2911fe6d63d217d55b53abbbd2911fcd13
Aditya Bobde <57266085+Adi-19@users.noreply.github.com>
2020-07-05 03:50:22 -0700

    Add files via upload

---
+++ Practice_Examples/Dict_Ex_1.py
@@ -0,0 +1,13 @@
+# WAP to count the number of fruits in the basket.
+# Use the dictionary and list to count the total number
+# of fruits, but do not count the other items from the basket.
+
+result = 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+for object, count in basket_items.items():
+   if object in fruits:
+       result += count
+
+print("There are {} fruits in the basket.".format(result))

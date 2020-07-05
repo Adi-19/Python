dd1f6d7e95c2d043736df3533f819c1490680533
Aditya Bobde <57266085+Adi-19@users.noreply.github.com>
2020-07-05 05:00:56 -0700

    Add files via upload

---
+++ Loops/for loop.py
@@ -0,0 +1,6 @@
+# WAP to show the use of for loop
+
+cities = ['new york city', 'mountain view', 'chicago', 'los angeles']
+for city in cities: #city is the iteration variable, cities is the iterable being looped over
+    print(city)
+print("Done!")

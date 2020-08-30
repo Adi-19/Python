2469f2954526142110a1a06202e250c923a93221
Aditya Bobde <57266085+Adi-19@users.noreply.github.com>
2020-08-30 08:22:28 -0700

    add done

---
+++ Functions/add/func add.py
@@ -0,0 +1,8 @@
+def total(a,b):
+          result = a+b
+          print("Sum of ", a, " and ", b ,"=",result)
+
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+
+total(a,b)

80e3278bd6d57b03b0bf74108a5bc99ee64f6f3a
Aditya Bobde <57266085+Adi-19@users.noreply.github.com>
2020-08-30 09:10:03 -0700

    Use of Global variable

---
+++ Functions/Global Variable/Global.py
@@ -0,0 +1,10 @@
+var = "Good"
+def show():
+    global var1
+    var1 = "morning"
+    print("In function var is", var)
+
+show()
+print("outside function var1 is ", var1)
+print("var is ",var)
+    

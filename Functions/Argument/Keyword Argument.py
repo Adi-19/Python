0027b29e22ca9bcf9dc0a4a5ea49bb2ef835c938
Aditya Bobde <57266085+Adi-19@users.noreply.github.com>
2020-09-02 07:51:36 -0700

    Keyboard argument done

---
+++ Functions/Argument/Keyword Argument.py
@@ -0,0 +1,7 @@
+# Prgram to demonstrate keyword argument
+def display(str,int_x,float_y):
+    print("The string is",str)
+    print("The interger is",int_x)
+    print("The float is",float_y)
+
+display(int_x =50,float_y=23.2,str="Aditya")

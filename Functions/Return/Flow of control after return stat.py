a606b52064790777d2e0cc765ca0b3e9afaacf4f
Aditya Bobde <57266085+Adi-19@users.noreply.github.com>
2020-09-02 20:04:25 +0530

    Added new file with bug fixed

--- Functions/Return/Flow of control after return stat.py
+++
@@ -1,8 +0,0 @@
-# Program to demonstrate flow of control after the return statement.
-def display():
-    print("In function")
-    print("About to extecute return statement")
-    return()
-print("This line will never be displayed")
-display()
-print("Back to the caller")

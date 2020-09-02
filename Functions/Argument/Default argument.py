a2f70696541f61915778e2dc9e65098cdf69af60
Aditya Bobde <57266085+Adi-19@users.noreply.github.com>
2020-09-02 08:01:52 -0700

    Default argument done

---
+++ Functions/Argument/Default argument.py
@@ -0,0 +1,8 @@
+# Prgram to demonstrate keyword argument
+def display(name,course="AI"):
+    print("The name is",name)
+    print("The course is",course)
+
+
+display(course="AI",name="adi")
+display(name="ram")

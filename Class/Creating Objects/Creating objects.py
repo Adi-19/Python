1decb1cd1319984ee0c8427c5873dcfca1f481d7
Aditya Bobde <57266085+Adi-19@users.noreply.github.com>
2020-09-07 21:53:22 -0700

    objects done

---
+++ Class/Creating Objects/Creating objects.py
@@ -0,0 +1,8 @@
+# Program to access class variables using class objects
+
+class ABC:
+    var = 10         # class variable
+
+obj =ABC()
+print(obj.var)       # class variable accessed using class object
+ 

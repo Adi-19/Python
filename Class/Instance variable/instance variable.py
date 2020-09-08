8ce51e5522a7cfb2c88644a6e19f36ef5878fbc7
Aditya Bobde <57266085+Adi-19@users.noreply.github.com>
2020-09-07 23:33:39 -0700

    Instance variable done

---
+++ Class/Instance variable/instance variable.py
@@ -0,0 +1,18 @@
+# Program illustrating the modifacation of an instance variable
+
+class Number:
+    even = 0
+    def check(self,num):
+        if num%2==0:
+            self.even = 1
+
+    def Even_Odd(self,num):
+        self.check(num)
+        
+        if self.even == 1:
+            print(num,"is even")
+        else:
+            print(num,"is odd")
+
+n = Number()
+n.Even_Odd(21)
